4c4f438b77814a9feb6f561aa16d803c79abc9b6
Ubuntu <ubuntu@ip-10-10-1-234.ap-south-1.compute.internal>
2025-04-04 22:43:01 +0530

    Auto commit on 2025-04-04 22:43:01

--- bookanytime-frontend/src/components/Header/SearchBar.jsx
+++ bookanytime-frontend/src/components/Header/SearchBar.jsx
@@ -343,7 +343,7 @@ const SearchBar = () => {
   };
 
   return (
-    <div className="search-page">
+    <div className="search-page container-fluid">
       <div className="search-inputs-container">
         <div className="search-bar-container left">
           <TextField

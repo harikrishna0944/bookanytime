794adf2f3e93e1883a4dafd15094e2197a10ae51
Ubuntu <ubuntu@ip-10-10-1-234.ap-south-1.compute.internal>
2025-04-04 22:58:01 +0530

    Auto commit on 2025-04-04 22:58:01

--- bookanytime-frontend/src/components/Header/SearchBar.jsx
+++ bookanytime-frontend/src/components/Header/SearchBar.jsx
@@ -407,7 +407,7 @@ const SearchBar = () => {
             </button>
           ))}
         </div>
-        <div className="gap-3 mb-3">
+        <div className="gap-3">
         <Button
           variant="primary"
           onClick={() => setShowFilterModal(true)}

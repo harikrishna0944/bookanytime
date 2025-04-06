66f4fd379ca67edc04ef1c2c3bc4dc019c5e245a
Ubuntu <ubuntu@ip-10-10-1-234.ap-south-1.compute.internal>
2025-04-06 20:57:01 +0530

    Auto commit on 2025-04-06 20:57:01

--- bookanytime-frontend/src/components/Header/SearchBar.jsx
+++ bookanytime-frontend/src/components/Header/SearchBar.jsx
@@ -411,12 +411,12 @@ const SearchBar = () => {
 
       </div>
       <div className="category-filters-container d-flex">
-      <div className="d-flex flex-nowrap gap-2 sticky-mobile col-xl-8 col-md-8 col-lg-8">
-          {["All", ...categories].map((category) => (
-            <button
+      <div className="d-flex flex-nowrap gap-2 category-buttons-wrapper col-xl-8 col-md-8 col-lg-8">
+        {["All", ...categories].map((category) => (
+          <button
             key={category}
             className={`btn category-btn ${
-              selectedCategories.includes(category) 
+              selectedCategories.includes(category)
                 ? "btn-primary rounded-0"
                 : "btn-outline-primary"
             }`}
@@ -428,8 +428,9 @@ const SearchBar = () => {
           >
             {category}
           </button>
-          ))}
-        </div>
+        ))}
+      </div>
+
         
         <div className="col-md-2 col-lg-2 col-xl-2 d-none d-md-block">
           <form className="d-flex" onSubmit={(e) => e.preventDefault()}>

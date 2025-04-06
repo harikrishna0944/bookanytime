e348cabc07fb9bc31469c66d68e324345881af0e
Ubuntu <ubuntu@ip-10-10-1-234.ap-south-1.compute.internal>
2025-04-06 20:30:01 +0530

    Auto commit on 2025-04-06 20:30:01

--- bookanytime-frontend/src/components/Header/SearchBar.jsx
+++ bookanytime-frontend/src/components/Header/SearchBar.jsx
@@ -470,7 +470,7 @@ const SearchBar = () => {
         </Button>
         
         <Dropdown className="custom-dropdown fs-13">
-          <Dropdown.Toggle variant="primary" id="dropdown-sort" className="p-2">
+          <Dropdown.Toggle variant="primary" id="dropdown-sort" className="p-1 p-lg-2">
             <FaSort className="me-2" />
             {getSortToggleText()}
           </Dropdown.Toggle>

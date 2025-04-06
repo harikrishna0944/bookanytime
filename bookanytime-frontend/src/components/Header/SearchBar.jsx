dc5f8238718f22d3317637e3cd035556754fb21d
Ubuntu <ubuntu@ip-10-10-1-234.ap-south-1.compute.internal>
2025-04-06 21:01:02 +0530

    Auto commit on 2025-04-06 21:01:02

--- bookanytime-frontend/src/components/Header/SearchBar.jsx
+++ bookanytime-frontend/src/components/Header/SearchBar.jsx
@@ -411,27 +411,25 @@ const SearchBar = () => {
 
       </div>
       <div className="category-filters-container d-flex">
-      <div className="d-flex flex-nowrap gap-2 category-buttons-wrapper col-xl-8 col-md-8 col-lg-8">
-        {["All", ...categories].map((category) => (
-          <button
-            key={category}
-            className={`btn category-btn ${
-              selectedCategories.includes(category)
-                ? "btn-primary rounded-0"
-                : "btn-outline-primary"
-            }`}
-            onClick={() => handleCategoryChange(category)}
-            style={{
-              transition: "all 0.3s ease",
-              minWidth: "80px"
-            }}
-          >
-            {category}
-          </button>
-        ))}
-      </div>
-
-        
+        <div className="d-flex flex-nowrap gap-2 category-buttons-wrapper col-xl-8 col-md-8 col-lg-8">
+          {["All", ...categories].map((category) => (
+            <button
+              key={category}
+              className={`btn category-btn ${
+                selectedCategories.includes(category)
+                  ? "btn-primary rounded-0"
+                  : "btn-outline-primary"
+              }`}
+              onClick={() => handleCategoryChange(category)}
+              style={{
+                transition: "all 0.3s ease",
+                minWidth: "80px"
+              }}
+            >
+              {category}
+            </button>
+          ))}
+        </div>
         <div className="col-md-2 col-lg-2 col-xl-2 d-none d-md-block">
           <form className="d-flex" onSubmit={(e) => e.preventDefault()}>
             <div className="input-group w-70">
